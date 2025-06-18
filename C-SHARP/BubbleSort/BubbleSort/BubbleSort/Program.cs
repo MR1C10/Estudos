8b6ac0adf3415422263c6a8848ac83a34e70c03a
MR1C10 <mauriciorodriguesoficial@gmail.com>
2025-06-18 06:12:13 -0300

    Add initial BubbleSort project files and configuration

---
+++ C-SHARP/BubbleSort/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lista = { 5, 3, 8, 4, 2, 6, 7, 34, 53, 213, 43, 654 };
+            
+            int n = lista.Length;
+
+            for (int i = 0; i < lista.Length; i++){
+                bool troca = false;
+                for (int j = 0; j < (lista.Length - 1); j++){
+                    if (lista[j] > lista[j + 1]){
+                        int temp = lista[j];
+                        lista[j] = lista[j + 1];
+                        lista[j + 1] = temp;
+                        troca = true;
+                    }
+                }
+
+                if (!troca) break;
+            }
+
+            for (int a = 0; a < (lista.Length); a++)
+               Console.WriteLine(lista[a]);
+        }
+
+        static void listaOrdenada(int[] lista, int size){
+            int i;
+            for (i = 0; i < size; i++){
+                Console.Write(lista[i] + " ");
+            }
+        }
+    }
+}
