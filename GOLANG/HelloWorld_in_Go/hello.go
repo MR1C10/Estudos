4973e7b55a19f66a68a7e0151dbcb58f27edcaef
Mauricio Rodrigues <mauriciorodriguesoficial@gmail.com>
2025-08-15 02:17:12 -0300

    testando variaveis e Formatação de saida

--- GOLANG/HelloWorld_in_Go/hello.go
+++ GOLANG/HelloWorld_in_Go/hello.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!");
+	fmt.Println("Hello World!")
+
+	var name string
+	var age int
+
+	name = "Mauricio"
+	age = 19
+
+	fmt.Printf("Name: %s, age: %d\n", name, age)
+
+	if age > 18 {
+		fmt.Println("Adult")
+	} else {
+		fmt.Println("Child/Teen")
+	}
 }
